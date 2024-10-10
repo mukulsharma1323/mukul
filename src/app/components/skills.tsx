6828cbf7672d0ee46dfd6dad6f6261f8896ee887
mukulsharma <mukulsharma1323@gmail.com>
2024-10-10 22:22:56 +0530

    skills

--- src/app/components/skills.tsx
+++ src/app/components/skills.tsx
@@ -1,4 +1,32 @@
 export default function Skills() {
+
+    const data = [
+        {
+            intro: "Web Design",
+            description: "Web design is the process of creating websites. It encompasses several different aspects, including webpage layout, content production, and graphic design."
+        },
+        {
+            intro: "Web Development",
+            description: "Web development refers to building, creating websites. It includes aspects such as web design, web publishing, web programming, and database management."
+        },
+        {
+            intro: "IoT",
+            description: "Internet of Things is a term that refers to anything connected to the Internet.In order for a device to be part of the IoT, it must be able to communicate with other devices."
+        },
+        {
+            intro: "Responsive Design",
+            description: "Responsive web design is about creating web pages that look good on all devices! A responsive web design will automatically adjust for different screen sizess"
+        },
+        {
+            intro: "Blockchain",
+            description: "Blockchain is a shared, immutable ledger that facilitates the process of recording transactions and tracking assets in a business network."
+        },
+        {
+            intro: "Cloud computing",
+            description: "Cloud computing is the on-demand availability of computer system resources, especially data storage and computing power.ie AWS and Google cloud"
+        }
+    ]
+
     return (
         <section id="service" className="services-mf route">
             <div className="container">
@@ -16,84 +44,24 @@ export default function Skills() {
                     </div>
                 </div>
                 <div className="row">
-                    <div className="col-md-4">
-                        <div className="service-box">
-                            <div className="service-ico">
-                                <span className="ico-circle"><i className="ion-monitor"></i></span>
+
+                    {data.map((item, index) => {
+                        return (
+                            <div className="col-md-4" key={index}>
+                                <div className="service-box">
+                                    <div className="service-ico">
+                                        <span className="ico-circle"><i className="ion-monitor"></i></span>
+                                    </div>
+                                    <div className="service-content">
+                                        <h2 className="s-title">{item?.intro}</h2>
+                                        <p className="s-description text-center">
+                                            {item?.description}
+                                        </p>
+                                    </div>
+                                </div>
                             </div>
-                            <div className="service-content">
-                                <h2 className="s-title">Web Design</h2>
-                                <p className="s-description text-center">
-                                    Web design is the process of creating websites. It encompasses several different aspects, including webpage layout, content production, and graphic design.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-md-4">
-                        <div className="service-box">
-                            <div className="service-ico">
-                                <span className="ico-circle"><i className="ion-code-working"></i></span>
-                            </div>
-                            <div className="service-content">
-                                <h2 className="s-title">Web Development</h2>
-                                <p className="s-description text-center">
-                                    Web development refers to building, creating websites. It includes aspects such as web design, web publishing, web programming, and database management.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-md-4">
-                        <div className="service-box">
-                            <div className="service-ico">
-                                <span className="ico-circle"><i className="ion-ios-home"></i></span>
-                            </div>
-                            <div className="service-content">
-                                <h2 className="s-title">IoT</h2>
-                                <p className="s-description text-center">
-                                    Internet of Things is a term that refers to anything connected to the Internet.In order for a device to be part of the IoT, it must be able to communicate with other devices.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-md-4">
-                        <div className="service-box">
-                            <div className="service-ico">
-                                <span className="ico-circle"><i className="ion-android-phone-portrait"></i></span>
-                            </div>
-                            <div className="service-content">
-                                <h2 className="s-title">Responsive Design</h2>
-                                <p className="s-description text-center">
-                                    Responsive web design is about creating web pages that look good on all devices! A responsive web design will automatically adjust for different screen sizess
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-md-4">
-                        <div className="service-box">
-                            <div className="service-ico">
-                                <span className="ico-circle"><i className="ion-social-bitcoin"></i></span>
-                            </div>
-                            <div className="service-content">
-                                <h2 className="s-title">Blockchain</h2>
-                                <p className="s-description text-center">
-                                    Blockchain is a shared, immutable ledger that facilitates the process of recording transactions and tracking assets in a business network.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-md-4">
-                        <div className="service-box">
-                            <div className="service-ico">
-                                <span className="ico-circle"><i className="ion-android-cloud"></i></span>
-                            </div>
-                            <div className="service-content">
-                                <h2 className="s-title">Cloud computing</h2>
-                                <p className="s-description text-center">
-                                    Cloud computing is the on-demand availability of computer system resources, especially data storage and computing power.ie AWS and Google cloud
-                                </p>
-                            </div>
-                        </div>
-                    </div>
+                        )
+                    })}
                 </div>
             </div>
         </section>
